8f074aec3f65caa4bb30fe8bd161187fd4916895
Kazuya Saso <kazsan0215@gmail.com>
2021-05-02 22:51:49 +0900

    recreate

--- house/urls.py
+++ house/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, indexfunc, reviewfunc, expensefunc, postreviewfunc, postexpensefunc, deletereviewfunc
 
 urlpatterns = [
-    path('', indexfunc, name='index'),
+    path('', listfunc, name='index'),
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
     path('list/', listfunc, name='list'),
