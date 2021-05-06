1a58331d5439a116b433266e3ec2206d92624ae3
Kazuya Saso <kazsan0215@gmail.com>
2021-05-06 10:28:10 +0900

    change settings

--- mhouse/settings.py
+++ mhouse/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'assets')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
