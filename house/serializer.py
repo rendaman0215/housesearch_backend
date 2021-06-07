7325e66d0ac5d7d40f130d4985910f391d3094a2
Kazuya Saso <kazsan0215@gmail.com>
2021-06-07 19:59:17 +0900

    change model

--- house/serializer.py
+++ house/serializer.py
@@ -6,7 +6,7 @@ class MakerCardSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
     class Meta:
         model = MakerCard
-        fields = '__all__' 
+        fields = ('pk', 'name', 'name_hira', 'name_kata', 'name_eng', 'image_url', 'get_review_count', 'get_expense_count', 'get_expense_avg', 'get_landarea_avg', 'get_rateavg', 'ratetostr', 'get_costavg', 'get_designavg', 'get_layoutavg', 'get_specavg', 'get_attachavg', 'get_guaranteeavg', 'get_salesavg')
     def get_image_url(self, maker):
         request = self.context.get('request')
         image_url = maker.images.url
@@ -15,14 +15,14 @@ class MakerCardSerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reviews
-        fields = '__all__' 
+        fields = ('author', 'status', 'costrate', 'costcomment', 'designrate', 'designcomment', 'layoutrate', 'layoutcomment', 'specrate', 'speccomment', 'attachrate', 'attachcomment', 'guaranteerate', 'guaranteecomment', 'salesrate', 'salescomment', 'avgrate', 'get_rateavg', 'maker_name', 'create_date')
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
     class Meta:
         model = Expense()
-        fields = '__all__' 
+        fields = ('author', 'status', 'cost', 'landarea', 'gradecomment', 'costupcomment', 'costdowncomment', 'image_url', 'maker_name', 'create_date')
     def get_image_url(self, expense):
         request = self.context.get('request')
         image_url = ""
