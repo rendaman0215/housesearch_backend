5f2fd1881d2998bdcfba36bf28f4bf30891460db
Kazuya Saso <kazsan0215@gmail.com>
2021-06-24 10:57:34 +0900

    add change serializer

--- house/serializer.py
+++ house/serializer.py
@@ -6,7 +6,7 @@ class MakerCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = MakerCard
         fields = ('pk', 'name', 'name_hira', 'name_kata', 'name_eng', 'image_url', 'get_review_count', 'get_expense_count', 'get_expense_avg', 'get_landarea_avg', 'get_rateavg', 'ratetostr', 'get_costavg', 'get_designavg', 'get_layoutavg', 'get_specavg', 'get_attachavg', 'get_guaranteeavg', 'get_salesavg')
-        read_only_fields = ('created_at',)
+        read_only_fields = ('pk','created_at',)
     def get_image_url(self, maker):
         request = self.context.get('request')
         image_url = maker.images.url
@@ -16,15 +16,15 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reviews
         fields = ('pk','author', 'status', 'costrate', 'costcomment', 'designrate', 'designcomment', 'layoutrate', 'layoutcomment', 'specrate', 'speccomment', 'attachrate', 'attachcomment', 'guaranteerate', 'guaranteecomment', 'salesrate', 'salescomment', 'avgrate', 'get_rateavg', 'maker_name', 'create_date')
-        read_only_fields = ('created_at',)
+        read_only_fields = ('pk','created_at',)
 
 
 class ExpenseSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
     class Meta:
-        model = Expense()
+        model = Expense
         fields = ('pk','author', 'status', 'cost', 'landarea', 'gradecomment', 'costupcomment', 'costdowncomment', 'image_url', 'maker_name', 'create_date')
-        read_only_fields = ('created_at',)
+        read_only_fields = ('pk','created_at',)
     def get_image_url(self, expense):
         request = self.context.get('request')
         image_url = ""
